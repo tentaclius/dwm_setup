c018eb7a11cbe822018d50e6b6c591073cb23912
tentaclius <tentaclius@gmail.com>
2019-03-13 20:53:04 +0300

    Changing volume controls to use an external script.

--- config.h
+++ config.h
@@ -72,8 +72,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-p", "command>", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
 static const char *lockscreencmd[] = { "gnome-screensaver-command", "-l", NULL };
-static const char *volumeincr[] = { "amixer", "set", "Master", "10%+", NULL };
-static const char *volumedecr[] = { "amixer", "set", "Master", "10%-", NULL };
+static const char *volumeincr[] = { "volume_up", NULL };
+static const char *volumedecr[] = { "volume_down", NULL };
 
 
 static Key keys[] = {
